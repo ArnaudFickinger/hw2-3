c2ef3ef8e76142c194b1b2524998fb8c5eed6b98
ArnaudFickinger <>
2021-03-17 07:32:46 -0700

    “a”

--- gpu.cu
+++ gpu.cu
@@ -74,61 +74,61 @@ void init_simulation(particle_t* parts, int num_parts, double size) {
     blks = (num_parts + NUM_THREADS - 1) / NUM_THREADS;
 
     /////
-    num_bins_side = int(size / cutoff);
-    num_bins = num_bins_side * num_bins_side;
-    size_bin = size / num_bins_side;
+    // num_bins_side = int(size / cutoff);
+    // num_bins = num_bins_side * num_bins_side;
+    // size_bin = size / num_bins_side;
     /////
 }
 
-__global__ void create_bin_counts(particle_t* parts, int num_parts, int* bin_counts, int size_bin, int num_bins) {
-    int tid = threadIdx.x + blockIdx.x * blockDim.x;
-    if (tid >= num_parts) {
-      return;
-    }
-    // particle_t& part = parts[tid];
-
-    // int bin_x = int(part.x / size_bin);
-    // int bin_y = int(part.y / size_bin);
-    // int bin_num = bin_x + bin_y * num_bins;
-
-    int bin_x = int(parts[tid].x / size_bin);
-    int bin_y = int(parts[tid].y / size_bin);
-    int bin_num = bin_x + bin_y * num_bins;
-
-    // int* cpu_bin_num = (int*) malloc(sizeof(int));
-    // cudaMemcpy(cpu_bin_num, bin_num, sizeof(int), cudaMemcpyDeviceToHost);
-
-    // std::cout << cpu_bin_num << ",\t";
-
-    bin_counts[bin_num] = 5;
-
-    // atomicAdd(&bin_counts[bin_num], 1);
-}
+// __global__ void create_bin_counts(particle_t* parts, int num_parts, int* bin_counts, int size_bin, int num_bins) {
+//     int tid = threadIdx.x + blockIdx.x * blockDim.x;
+//     if (tid >= num_parts) {
+//       return;
+//     }
+//     // particle_t& part = parts[tid];
+//
+//     // int bin_x = int(part.x / size_bin);
+//     // int bin_y = int(part.y / size_bin);
+//     // int bin_num = bin_x + bin_y * num_bins;
+//
+//     int bin_x = int(parts[tid].x / size_bin);
+//     int bin_y = int(parts[tid].y / size_bin);
+//     int bin_num = bin_x + bin_y * num_bins;
+//
+//     // int* cpu_bin_num = (int*) malloc(sizeof(int));
+//     // cudaMemcpy(cpu_bin_num, bin_num, sizeof(int), cudaMemcpyDeviceToHost);
+//
+//     // std::cout << cpu_bin_num << ",\t";
+//
+//     bin_counts[bin_num] = 5;
+//
+//     // atomicAdd(&bin_counts[bin_num], 1);
+// }
 
 void simulate_one_step(particle_t* parts, int num_parts, double size) {
     // parts live in GPU memory
     // Rewrite this function
 
     /////
-    if (!parts_ordered_inds) {
-        cudaMalloc((void**) &parts_ordered_inds, size * sizeof(int));
-    }
-
-    if (!bin_counts) {
-        cudaMalloc((void**) &bin_counts, num_bins * sizeof(int));
-    }
-
-    cudaMemset(parts_ordered_inds, -1, size * sizeof(int));
-    cudaMemset(bin_counts, 0, num_bins * sizeof(int));
-
-    int* cpu_bin_counts = (int*) malloc(num_bins * sizeof(int));
-    cudaMemcpy(cpu_bin_counts, bin_counts, num_bins * sizeof(int), cudaMemcpyDeviceToHost);
-
-    for (int i = 0; i < 2; i++) {
-        std::cout << i << ": "<<  cpu_bin_counts[i] << ",\t";
-    }
-
-    create_bin_counts<<<blks, NUM_THREADS>>>(parts, num_parts, bin_counts, size_bin, num_bins);
+    // if (!parts_ordered_inds) {
+    //     cudaMalloc((void**) &parts_ordered_inds, size * sizeof(int));
+    // }
+    //
+    // if (!bin_counts) {
+    //     cudaMalloc((void**) &bin_counts, num_bins * sizeof(int));
+    // }
+    //
+    // cudaMemset(parts_ordered_inds, -1, size * sizeof(int));
+    // cudaMemset(bin_counts, 0, num_bins * sizeof(int));
+    //
+    // int* cpu_bin_counts = (int*) malloc(num_bins * sizeof(int));
+    // cudaMemcpy(cpu_bin_counts, bin_counts, num_bins * sizeof(int), cudaMemcpyDeviceToHost);
+    //
+    // for (int i = 0; i < 2; i++) {
+    //     std::cout << i << ": "<<  cpu_bin_counts[i] << ",\t";
+    // }
+    //
+    // create_bin_counts<<<blks, NUM_THREADS>>>(parts, num_parts, bin_counts, size_bin, num_bins);
     /////
 
     // Compute forces
