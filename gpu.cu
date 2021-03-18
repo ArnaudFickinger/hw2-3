c05646e1e4cc85cf6f11690442e0211f8d9a96ee
ArnaudFickinger <>
2021-03-17 22:34:01 -0700

    “b”

--- gpu.cu
+++ gpu.cu
@@ -171,7 +171,7 @@ __global__ void order_particle(particle_t* parts, int num_parts, float size_bin_
 
     // needs to be atomic
     atomicAdd(&bin_counts_incremental_device_[bin_num], 1);
-    // __syncthreads();
+    __syncthreads();
     int index = bin_counts_incremental_device_[bin_num] - 1;
     // cudaMemset(&ordered_particles_device_[index], tid, sizeof(int));
     ordered_particles_device_[index] = tid;
