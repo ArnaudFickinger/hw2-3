7e29a01d2b0de5d32343d6a54d9c57eaccfac812
ArnaudFickinger <>
2021-03-17 21:08:37 -0700

    “a”

--- gpu.cu
+++ gpu.cu
@@ -140,27 +140,27 @@ __global__ void update_bin_counts_test(particle_t *parts, int num_parts, int *bi
     // atomicAdd(&bin_counts[bin_num], 1);
 }
 
-__global__ void scan(float *g_odata, float *g_idata, int n)
-{
- extern __shared__ float temp[]; // allocated on invocation
- int thid = threadIdx.x;
- int pout = 0, pin = 1;
- // load input into shared memory.
- // This is exclusive scan, so shift right by one and set first elt to 0
- temp[pout*n + thid] = (thid > 0) ? g_idata[thid-1] : 0;
- __syncthreads();
- for (int offset = 1; offset < n; offset *= 2)
- {
- pout = 1 - pout; // swap double buffer indices
- pin = 1 - pout;
- if (thid >= offset)
- temp[pout*n+thid] += temp[pin*n+thid - offset];
- else
- temp[pout*n+thid] = temp[pin*n+thid];
- __syncthreads();
- }
- g_odata[thid] = temp[pout*n+thid1]; // write output
-}
+// __global__ void scan(float *g_odata, float *g_idata, int n)
+// {
+//  extern __shared__ float temp[]; // allocated on invocation
+//  int thid = threadIdx.x;
+//  int pout = 0, pin = 1;
+//  // load input into shared memory.
+//  // This is exclusive scan, so shift right by one and set first elt to 0
+//  temp[pout*n + thid] = (thid > 0) ? g_idata[thid-1] : 0;
+//  __syncthreads();
+//  for (int offset = 1; offset < n; offset *= 2)
+//  {
+//  pout = 1 - pout; // swap double buffer indices
+//  pin = 1 - pout;
+//  if (thid >= offset)
+//  temp[pout*n+thid] += temp[pin*n+thid - offset];
+//  else
+//  temp[pout*n+thid] = temp[pin*n+thid];
+//  __syncthreads();
+//  }
+//  g_odata[thid] = temp[pout*n+thid1]; // write output
+// }
 
 void init_simulation(particle_t* parts, int num_parts, double size) {
     // You can use this space to initialize data objects that you may need
