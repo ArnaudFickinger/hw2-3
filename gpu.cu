f2950d7f3aaad5791f849e8577e1de3e3a7b4f68
ashankv <ashank.verma@gmail.com>
2021-03-19 14:09:16 -0700

    Modify cuda

--- gpu.cu
+++ gpu.cu
@@ -420,6 +420,9 @@ void simulate_one_step(particle_t* parts, int num_parts, double size) {
     // Rewrite this function
 
     update_bin_counts<<<blks, NUM_THREADS>>>(parts, num_parts, bin_counts_dev, size_bin, num_bins_1d, num_bins);
+
+    // DEBUG BIN COUNTS
+    std::cout << "BIN COUNTS" << std::endl;
     cudaMemcpy(bin_counts_host, bin_counts_dev, sizeof(int) * num_bins, cudaMemcpyDeviceToHost);
     for (int i = 0; i < num_bins; i++) {
         std::cout << bin_counts_host[i] << std::endl;
