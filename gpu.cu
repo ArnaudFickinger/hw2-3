d3ff2c1d866f95d27416e74a80dad9ce71dbcd75
Alan Liang <alan_liang@outlook.com>
2021-03-18 22:17:22 -0700

    Test

--- gpu.cu
+++ gpu.cu
@@ -409,7 +409,7 @@ __global__ void update_bin_counts(particle_t* parts, int num_parts, int* bin_cou
     int bin_x = int(parts[tid].x / size_bin);
     int bin_y = int(parts[tid].y / size_bin);
     int bin_num = bin_x + bin_y * num_bins;
-
+    std::cout << bin_num << std::endl;
     atomicAdd(&bin_counts[bin_num], 1);
 }
 
