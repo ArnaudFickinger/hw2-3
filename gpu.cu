0966d7709b68eaeeb351800c54a8801a4e9a5a9f
ashankv <ashank.verma@gmail.com>
2021-03-19 13:39:58 -0700

    Modify cuda

--- gpu.cu
+++ gpu.cu
@@ -429,6 +429,8 @@ void simulate_one_step(particle_t* parts, int num_parts, double size) {
         std::cout << bin_counts_host[i] << std::endl;
     }
 
+    std::cout << "end step" << std::endl;
+
     // Compute forces
     // compute_forces_gpu<<<blks, NUM_THREADS>>>(parts, num_parts);
 
