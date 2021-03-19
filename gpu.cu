35657e6944668818690cfb5f8befa2993f885f5f
Alan Liang <alan_liang@outlook.com>
2021-03-18 22:37:07 -0700

    Test

--- gpu.cu
+++ gpu.cu
@@ -420,6 +420,7 @@ void simulate_one_step(particle_t* parts, int num_parts, double size) {
 
     update_bin_counts<<<blks, NUM_THREADS>>>(parts, num_parts, bin_counts_host, size_bin, num_bins_1d, num_bins);
 
+    cudaDeviceSynchronize(); 
     // std::cout << bin_counts_host[0] << std::endl;
     for (int i = 0; i < num_bins; i++) {
         std::cout << bin_counts_host[i] << std::endl;
